211f6abfe7d7f7ba94ac2aba46c8c4c89f42bdcd
Alex Toth <tothalex95@gmail.com>
2019-04-02 13:54:59 +0200

    load more books on scroll event

--- app/src/main/java/hu/miskolc/uni/iit/googlebookapp/MainActivity.kt
+++ app/src/main/java/hu/miskolc/uni/iit/googlebookapp/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import hu.miskolc.uni.iit.googlebookapp.domain.model.SearchResult
 import hu.miskolc.uni.iit.googlebookapp.domain.usecase.GetBooks
 import hu.miskolc.uni.iit.googlebookapp.presentation.adapter.SearchResultAdapter
 import hu.miskolc.uni.iit.googlebookapp.presentation.viewmodel.SearchResultViewModel
@@ -21,6 +23,23 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
     private val viewModel by inject<SearchResultViewModel>()
     private val adapter = SearchResultAdapter()
 
+    private var searchButtonClicked = false
+    private var isLoading = false
+    private val searchResultObserver = Observer<SearchResult> {
+        if (searchButtonClicked) {
+            (application as GoogleBookApp).searchResult.items.clear()
+            adapter.searchResult.items.clear()
+        }
+
+        (application as GoogleBookApp).searchResult.items.addAll(it.items)
+        adapter.searchResult.items.addAll(it.items)
+
+        adapter.notifyDataSetChanged()
+
+        isLoading = false
+        progress_bar.visibility = View.GONE
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,28 +47,44 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
         search_results_recycler_view.layoutManager = LinearLayoutManager(this)
         search_results_recycler_view.adapter = adapter
 
+        viewModel.searchResultLiveData.observe(this, searchResultObserver)
+
         search_button.setOnClickListener {
+            searchButtonClicked = true
+            isLoading = true
             progress_bar.visibility = View.VISIBLE
-            search_results_recycler_view.visibility = View.GONE
-            viewModel.getSearchResult(
+            viewModel.search(
                 GetBooks.Params(
                     search_edit_text.text.toString(),
                     10
                 )
             )
-                .observe(this, Observer {
-                    (application as GoogleBookApp).searchResult.items.clear()
-                    (application as GoogleBookApp).searchResult.items.addAll(it.items)
-
-                    adapter.searchResult.items.clear()
-                    adapter.searchResult.items.addAll(it.items)
-
-                    adapter.notifyDataSetChanged()
-
-                    progress_bar.visibility = View.GONE
-                    search_results_recycler_view.visibility = View.VISIBLE
-                })
         }
+
+        search_results_recycler_view.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                val fetchedItemCount = recyclerView.layoutManager?.itemCount
+                val lastVisibleItemPosition =
+                    (recyclerView.layoutManager as LinearLayoutManager).findLastVisibleItemPosition()
+
+                if (dy > 0
+                    && fetchedItemCount == lastVisibleItemPosition + 1
+                    && !isLoading
+                ) {
+                    searchButtonClicked = false
+                    isLoading = true
+                    progress_bar.visibility = View.VISIBLE
+
+                    viewModel.search(
+                        GetBooks.Params(
+                            search_edit_text.text.toString(),
+                            maxResults = 10,
+                            startIndex = fetchedItemCount
+                        )
+                    )
+                }
+            }
+        })
     }
 
     override fun getLifecycle(): Lifecycle {
--- app/src/main/java/hu/miskolc/uni/iit/googlebookapp/presentation/viewmodel/SearchResultViewModel.kt
+++ app/src/main/java/hu/miskolc/uni/iit/googlebookapp/presentation/viewmodel/SearchResultViewModel.kt
@@ -1,6 +1,5 @@
 package hu.miskolc.uni.iit.googlebookapp.presentation.viewmodel
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import hu.miskolc.uni.iit.googlebookapp.domain.model.SearchResult
 import hu.miskolc.uni.iit.googlebookapp.domain.usecase.GetBooks
@@ -9,15 +8,14 @@ class SearchResultViewModel(
     private val getBooks: GetBooks
 ) : BaseViewModel(getBooks) {
 
-    private val searchResultLiveData = MutableLiveData<SearchResult>()
+    val searchResultLiveData = MutableLiveData<SearchResult>()
 
-    fun getSearchResult(query: GetBooks.Params): LiveData<SearchResult> {
+    fun search(query: GetBooks.Params) {
         getBooks(
-            onSuccess = { searchResultLiveData.value = it},
+            onSuccess = { searchResultLiveData.value = it },
             onError = {},
             params = query
         )
-        return searchResultLiveData
     }
 
 }
